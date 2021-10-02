7a6ef00ca9afddacf63364a6026adc50293f0acc
renatavagapova <vagapova.rena@list.ru>
2021-10-02 13:35:04 +0300

    Выполнено первое задание

--- lesson1/Program.cs
+++ lesson1/Program.cs
@@ -6,22 +6,15 @@ namespace lesson1
     {
         //Задание 1
         //проверка числа на прсототу согласно блок-схеме
-        static int Simple (int n)
+        static int IsSimple (int n)
         {
             int d = 0;
-            for (int i = 2; i < n; i++)
+            int i = 2;
+            while (i < n)
             {
                 if (n % i == 0)
-                {
                     d++;
-                    i++;
-
-                }
-                else
-                {
-                    i++;
-                }
-
+                i++;
             }
             if (d == 0)
             {
@@ -32,7 +25,7 @@ namespace lesson1
                 Console.WriteLine("Число не простое");
             }
             return n;
-
+            
         }
         
 
@@ -40,7 +33,7 @@ namespace lesson1
         //Сложность функции равна O(N^3)
         public static int StrangeSum(int[] inputArray)
         {
-            int sum = 0;                                              //О(1)
+            int sum = 0;                                              //o(1)
             for (int i = 0; i < inputArray.Length; i++)               //O(n)
             {
                 for (int j = 0; j < inputArray.Length; j++)           //O(n)
@@ -97,20 +90,32 @@ namespace lesson1
             }
             return cur;
         }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Задание 1");
             Console.WriteLine("Введите число");
             int n = int.Parse(Console.ReadLine());
-            Simple(n);
+            IsSimple(n); 
+            Console.WriteLine();
 
-            Console.WriteLine("Введите число, до которого будет считаться ряд Фибоначчи ");
+            Console.WriteLine("Задание 3");    
+            Console.WriteLine("Ряд Фибоначчи, вычесленный с помощью рекурсии \nВведите число, до которого будет считаться ряд Фибоначчи ");
             int number = int.Parse(Console.ReadLine());
             for (int i = 0; i <= number; i++)
             {
                 Console.Write(FibonacciRecurs(i) + " ");
             }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Ряд Фибоначчи, вычесленный с помощью циклов \nВведите число, до которого будет считаться ряд Фибоначчи ");
+            int number1 = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= number1; i++)
+            {
+                Console.Write(FibonacciByCicl(i) + " ");
+            }
             Console.ReadKey();
-            //нужно еще организовать вывод для Фибоначчи с помощью цикла и пересмотреть первую задачу.
         }
     }
 }
