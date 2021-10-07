635e9128b5ac94137f51f4fa1b57bc1645e2fbd5
renatavagapova <vagapova.rena@list.ru>
2021-10-07 17:39:42 +0300

    Добавлено задание 2, курс алгоритмов

--- lesson1/Program.cs
+++ lesson1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace lesson1
 {
@@ -6,7 +8,7 @@ namespace lesson1
     {
         //Задание 1
         //проверка числа на прсототу согласно блок-схеме
-        static int IsSimple (int n)
+        static int IsSimple(int n)
         {
             int d = 0;
             int i = 2;
@@ -25,35 +27,72 @@ namespace lesson1
                 Console.WriteLine("Число не простое");
             }
             return n;
-            
         }
-        
-
-        //Задание 2
-        //Сложность функции равна O(N^3)
-        public static int StrangeSum(int[] inputArray)
+        public class TestCase
         {
-            int sum = 0;                                              //o(1)
-            for (int i = 0; i < inputArray.Length; i++)               //O(n)
+            public int X { get; set; }
+            public string Expected { get; set; }
+            public Exception ExpectedException { get; set; }
+        }
+
+        static void TestIsSimple(TestCase testCase)
+        {
+            try
             {
-                for (int j = 0; j < inputArray.Length; j++)           //O(n)
+                var actual = IsSimple(testCase.X);
+                var a = Convert.ToString(actual);
+
+                if (actual == testCase.Expected)
                 {
-                    for (int k = 0; k < inputArray.Length; k++)       //O(n)
-                    {
-                        int y = 0;
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
 
-                        if (j != 0)
-                        {
-                            y = k / j;                                //O(1)
-                        }
-
-                        sum += inputArray[i] + i + k + j + y;         //O(1)
-                    }
+            }
+            catch (Exception ex)
+            {
+                if (testCase.ExpectedException != null)
+                {
+                    //TODO add type exception tests;
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
                 }
             }
 
-            return sum;                                               //O(1)
+
         }
+
+        ////Задание 2
+        ////Сложность функции равна O(N^3)
+        //public static int StrangeSum(int[] inputArray)
+        //{
+        //    int sum = 0;                                              //o(1)
+        //    for (int i = 0; i < inputArray.Length; i++)               //O(n)
+        //    {
+        //        for (int j = 0; j < inputArray.Length; j++)           //O(n)
+        //        {
+        //            for (int k = 0; k < inputArray.Length; k++)       //O(n)
+        //            {
+        //                int y = 0;
+
+        //                if (j != 0)
+        //                {
+        //                    y = k / j;                                //O(1)
+        //                }
+
+        //                sum += inputArray[i] + i + k + j + y;         //O(1)
+        //            }
+        //        }
+        //    }
+
+        //    return sum;                                               //O(1)
+        //}
 
         //Задание 3
         //Вычисление чисел Фибоначчи рекурсивно
@@ -69,53 +108,130 @@ namespace lesson1
             }
         }
 
-        //Вычисление числа Фибоначчи с помощью цикла
-        public static int FibonacciByCicl(int n)
-        {
-            if (n==0)
-            {
-                return 0;
-            }
-            if (n==1)
-            {
-                return 1;
-            }
-            var prev = 0;
-            var cur = 1;
-            for (var i = 2; i <= n; i++) 
-            {
-                var temp = prev + cur;
-                prev = cur;
-                cur = temp;
-            }
-            return cur;
-        }
+
+        ////Вычисление числа Фибоначчи с помощью цикла
+        //public static int FibonacciByCicl(int n)
+        //{
+        //    if (n == 0)
+        //    {
+        //        return 0;
+        //    }
+        //    if (n == 1)
+        //    {
+        //        return 1;
+        //    }
+        //    var prev = 0;
+        //    var cur = 1;
+        //    for (var i = 2; i <= n; i++)
+        //    {
+        //        var temp = prev + cur;
+        //        prev = cur;
+        //        cur = temp;
+        //    }
+        //    return cur;
+        //}
 
         static void Main(string[] args)
         {
             Console.WriteLine("Задание 1");
-            Console.WriteLine("Введите число");
-            int n = int.Parse(Console.ReadLine());
-            IsSimple(n); 
-            Console.WriteLine();
-
-            Console.WriteLine("Задание 3");    
-            Console.WriteLine("Ряд Фибоначчи, вычесленный с помощью рекурсии \nВведите число, до которого будет считаться ряд Фибоначчи ");
-            int number = int.Parse(Console.ReadLine());
-            for (int i = 0; i <= number; i++)
+            //Console.WriteLine("Введите число");
+            //int n = int.Parse(Console.ReadLine());
+            //IsSimple(n);
+            //Console.WriteLine();
+            var testCase1 = new TestCase()
             {
-                Console.Write(FibonacciRecurs(i) + " ");
-            }
-            Console.WriteLine();
-            Console.WriteLine();
+                X = 5,
+                Expected = 5,
+                ExpectedException = null
+            };
+            TestIsSimple(testCase1);
 
-            Console.WriteLine("Ряд Фибоначчи, вычесленный с помощью циклов \nВведите число, до которого будет считаться ряд Фибоначчи ");
-            int number1 = int.Parse(Console.ReadLine());
-            for (int i = 0; i <= number1; i++)
-            {
-                Console.Write(FibonacciByCicl(i) + " ");
-            }
-            Console.ReadKey();
+
+            //Console.WriteLine("Задание 3");
+            //Console.WriteLine("Ряд Фибоначчи, вычесленный с помощью рекурсии \nВведите число, до которого будет считаться ряд Фибоначчи ");
+            //int number = int.Parse(Console.ReadLine());
+            //for (int i = 0; i <= number; i++)
+            //{
+            //    Console.Write(FibonacciRecurs(i) + " ");
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+            //Console.WriteLine("Ряд Фибоначчи, вычесленный с помощью циклов \nВведите число, до которого будет считаться ряд Фибоначчи ");
+            //int number1 = int.Parse(Console.ReadLine());
+            //for (int i = 0; i <= number1; i++)
+            //{
+            //    Console.Write(FibonacciByCicl(i) + " ");
+            //}
+            //Console.ReadKey();
         }
+        //public class TestCase
+        //{
+        //    public int X { get; set; }
+        //    public int Y { get; set; }
+        //    public int Expected { get; set; }
+        //    public Exception ExpectedException { get; set; }
+        //}
+
+        //static int SumPositive(int x, int y)
+        //{
+        //    if (x < 0 || y < 0)
+        //    {
+        //        throw new ArgumentException("x and y must be positive");
+        //    }
+
+        //    return x + y;
+        //}
+
+        //static void TestSum(TestCase testCase)
+        //{
+        //    try
+        //    {
+        //        var actual = SumPositive(testCase.X, testCase.Y);
+
+        //        if (actual == testCase.Expected)
+        //        {
+        //            Console.WriteLine("VALID TEST");
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("INVALID TEST");
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        if (testCase.ExpectedException != null)
+        //        {
+        //            //TODO add type exception tests;
+        //            Console.WriteLine("VALID TEST");
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("INVALID TEST");
+        //        }
+        //    }
+        //}
+
+        //static void Main(string[] args)
+        //{
+        //    var testCase1 = new TestCase()
+        //    {
+        //        X = 6,
+        //        Y = 4,
+        //        Expected = 10,
+        //        ExpectedException = null
+        //    };
+
+        //    var testCase2 = new TestCase()
+        //    {
+        //        X = 1,
+        //        Y = 4,
+        //        Expected = 5,
+        //        ExpectedException = null
+        //    };
+
+        //    TestSum(testCase1);
+        //    TestSum(testCase2);
+        //}
     }
 }
---
+++ lesson2/Program.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace lesson2
+{
+    class BinarySearch
+    {
+        public static int Binary(int[] array, int searchedValue, int first, int last)
+        {
+            //границы сошлись
+            if (first > last)
+            {
+                //элемент не найден
+                return -1;
+            }
+
+            //средний индекс подмассива
+            var middle = (first + last) / 2;
+            //значение в средине подмассива
+            var middleValue = array[middle];
+
+            if (middleValue == searchedValue)
+            {
+                return middle;
+            }
+            else
+            {
+                if (middleValue > searchedValue)
+                {
+                    //рекурсивный вызов поиска для левого подмассива
+                    return Binary(array, searchedValue, first, middle - 1);
+                }
+                else
+                {
+                    //рекурсивный вызов поиска для правого подмассива
+                    return Binary(array, searchedValue, middle + 1, last);
+                }
+            }
+        }
+        class Program
+        {
+            #region Задания
+            /*
+             * Задание 1
+             * Требуется реализовать класс двусвязного списка и выполнить операции:
+             * - вставки
+             * - удаления 
+             * - поиска элемента в нём
+             * __________________________________
+             * Задание 2
+             * Требуется написать функцию бинарного поиска, 
+             * посчитать его асимптотическую сложность и 
+             * проверить работоспособность функции.
+
+             */
+            #endregion
+
+
+            public class Node : ILinkedList
+            {
+                public int Value { get; set; }
+                public Node NextNode { get; set; }
+                public Node PrevNode { get; set; }
+
+                public Node firstNode;//первый(стартовый) элемент
+                public Node lastNode;//последний(конечный) элемент
+                int count;//кол-во элементов всписке
+
+
+                public int GetCount() => count;
+
+                public void AddNode(int value)
+                {
+                    var newNode = new Node { Value = value };
+
+                    if (firstNode == null)
+                    {
+                        firstNode = lastNode = newNode;
+                    }
+                    else
+                    {
+                        lastNode.NextNode = newNode;
+                        lastNode = newNode;
+                    }
+                    count++;
+                }
+
+                public void AddNodeAfter(Node node, int value)
+                {
+                    if (value > count || value < 0)
+                        throw new ArgumentOutOfRangeException();
+                    else
+                    {
+                        var next = node.NextNode;
+                        var newOne = new Node() { Value = value, NextNode = next, PrevNode = node };
+                        node.NextNode = newOne;
+                        if (next != null) next.PrevNode = newOne;
+                        count++;
+                    }
+                }
+
+                private Node GetNodeOnIndex(int index)
+                {
+                    if (IsIndexNotInRange(index)) return null;
+
+                    Node needed;
+
+                    if (count - 1 / 2 >= index)
+                    {
+                        needed = firstNode;
+                        for (var i = 1; i <= index; i++)
+                        {
+                            needed = needed.NextNode;
+                        }
+                    }
+                    else
+                    {
+                        needed = lastNode;
+                        for (var i = count - 1; i > index; i--)
+                        {
+                            needed = needed.PrevNode;
+                        }
+                    }
+
+                    return needed;
+                }
+
+                public void RemoveNode(int index)
+                {
+                    if (IsIndexNotInRange(index))
+                        throw new ArgumentOutOfRangeException();
+
+                    if (index == 0)
+                    {
+                        if (firstNode.NextNode is { } node)
+                        {
+                            firstNode = node;
+                            node.PrevNode = null;
+                        }
+                        else firstNode = lastNode = null;
+                    }
+                    else
+                    {
+                        var toRemove = GetNodeOnIndex(index);
+
+                        if (toRemove.NextNode != null)
+                            toRemove.NextNode.PrevNode = toRemove.PrevNode;
+                        toRemove.PrevNode.NextNode = toRemove.NextNode;
+                        toRemove.NextNode = toRemove.PrevNode = null;
+                    }
+                    count--;
+                }
+
+
+
+                private bool IsIndexNotInRange(int index) => index < 0 || index >= count;
+                public void RemoveNode(Node node)
+                {
+                    
+                    if (firstNode == node)
+                    {
+                        if (firstNode == lastNode)
+                        {
+                            firstNode = lastNode = null;
+                        }
+                        else
+                        {
+                            firstNode = node.NextNode;
+                            node.NextNode = null;
+                            firstNode.PrevNode = null;
+                        }
+                    }
+                    else
+                    {
+                        if (node.NextNode != null)
+                    node.NextNode.PrevNode = node.PrevNode;
+                        node.PrevNode.NextNode = node.NextNode;
+                        node.NextNode = node.PrevNode = null;
+                    }
+
+                }
+
+
+                public Node FindNode(Node node, int searchValue)
+                {
+                    var currentnode = node.firstNode;
+
+                    while (currentnode != null)
+                    {
+                        if (currentnode.Value == searchValue)
+                        {
+                            return currentnode;
+                        }
+                        else
+                        {
+                            currentnode = currentnode.NextNode;
+                        }
+                    }
+                    return null;
+                }
+
+               
+
+             
+            }
+
+
+            //Начальную и конечную ноду нужно хранить в самой реализации интерфейса
+            public interface ILinkedList
+            {
+                int GetCount(); // возвращает количество элементов в списке  
+                void AddNode(int value);  // добавляет новый элемент списка             
+                void AddNodeAfter(Node node, int value); // добавляет новый элемент списка после определённого элемента
+                void RemoveNode(int index); // удаляет элемент по порядковому номеру
+                void RemoveNode(Node node); // удаляет указанный элемент
+                Node FindNode(Node node, int searchValue); // ищет элемент по его значению
+               
+            }
+
+
+
+
+            static void Main(string[] args)
+            {
+                
+
+
+                Console.WriteLine("Задание 2");
+                int[] array = new int[] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21 };
+                Console.WriteLine("Введите искомый целочисленный элемент в массиве");
+                int key = int.Parse(Console.ReadLine());
+                var searchResult = Binary(array, key, 0, array.Length - 1);
+                if (searchResult < 0)
+                {
+                    Console.WriteLine("Элемент со значением {0} не найден", key);
+                }
+                else
+                {
+                    Console.WriteLine("Элемент найден. Индекс элемента со значением {0} равен {1}", key, searchResult);
+                }
+
+
+            }
+
+
+        }
+
+    }
+}
+
