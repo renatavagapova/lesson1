a8296d50f8158ee4a136c976f2ff09a1bf1ca35e
renatavagapova <vagapova.rena@list.ru>
2021-10-20 20:08:41 +0300

    Практическое задание 6, курс алгоритмов

---
+++ lesson-6/Edge.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lesson_6
+{
+    class Edge
+    {
+        public Vertex From { get; set; }
+
+        public Vertex To { get; set; }
+
+        public int Weight { get; set; }
+
+        public Edge(Vertex from, Vertex to, int weight = 1)
+        {
+            From = from;
+            To = to;
+            Weight = weight;
+        }
+
+        public override string ToString()
+        {
+            return $"({From};{To})";
+        }
+    }
+}
---
+++ lesson-6/Graph.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lesson_6
+{
+    class Graph
+    {
+        List<Vertex> Vertexes = new List<Vertex>();
+        List<Edge> Edges = new List<Edge>();
+
+        public int VertexCount => Vertexes.Count;
+        public int EdgeCount => Edges.Count;
+
+        public void AddVertex(Vertex vertex)
+        {
+            Vertexes.Add(vertex);
+        }
+
+        public void AddEdge(Vertex from, Vertex to)
+        {
+            var edge = new Edge(from, to);
+            Edges.Add(edge);
+
+        }
+
+        public int[,] GetMatrix()
+        {
+            var matrix = new int[Vertexes.Count, Vertexes.Count];
+
+            foreach (var edge in Edges)
+            {
+                var row = edge.From.Number;
+                var column = edge.To.Number;
+
+                matrix[row, column] = edge.Weight;
+            }
+            return matrix;
+        }
+
+        public List<Vertex> GetVetexLists(Vertex vertex)
+        {
+            var result = new List<Vertex>();
+
+            foreach (var edge in Edges)
+            {
+                if (edge.From == vertex)
+                {
+                    result.Add(edge.To);
+                }
+            }
+            return result;
+        }
+
+        public bool Wave(Vertex start, Vertex finish)
+        {
+            //var result = new List<Vertex>();
+
+            var list = new List<Vertex>
+            {
+                start
+            };
+
+            list.Add(start);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                var vertex = list[i];
+                foreach (var v in GetVetexLists(vertex))
+                {
+                    if (!list.Contains(v))
+                    {
+                        list.Add(v);
+
+                    }
+                }
+
+            }
+
+            return list.Contains(finish);
+        }
+
+    }
+}
